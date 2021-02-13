1f319b3dee4a8518aa34d682c596dbb1fc33b636
anandu <pavanan.anandu63@gmail.com>
2021-02-13 14:13:08 +0530

    added ctrl-c to quit the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io::{self, Read};
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Building Papyrust!");
+  println!("Papyrust running..");
+  
+  let _raw_mode_binder = io::stdout().into_raw_mode().unwrap();
+  for bytes in io::stdin().bytes() {
+    
+    // variable shadowing 
+    
+    match bytes {
+      Ok(bytes) => {
+        let character = bytes as char;
+        if character.is_control() {
+          println!("{:#b} \r", bytes);
+        } else {
+          println!("{:#b} => {} \r", bytes, character);
+        }
+        if bytes == get_control_byte('q') {
+          break;
+        }
+      }
+      Err(err) => {
+        die(err);
+      }
+    }
+  }
+}
+
+fn get_control_byte(character: char) -> u8 {
+  let character = character as u8;
+  character & 0b0001_1111
+}
+
+fn die(err: io::Error) {
+  panic!(err)
 }
